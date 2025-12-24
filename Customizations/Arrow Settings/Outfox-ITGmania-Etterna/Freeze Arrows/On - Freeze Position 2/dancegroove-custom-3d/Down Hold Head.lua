cf7f0a746a9468df8202ab0901d87c66c587b125
PoRa-dayo <62925753+PoRa-dayo@users.noreply.github.com>
2025-12-25 00:41:56 +0700

    more sm5

---
+++ Customizations/Arrow Settings/Outfox-ITGmania-Etterna/Freeze Arrows/On - Freeze Position 2/dancegroove-custom-3d/Down Hold Head.lua
@@ -0,0 +1,8 @@
+local t = Def.Model {
+	Meshes=NOTESKIN:GetPath('','arrowmesh');
+	Materials=NOTESKIN:GetPath('','_Down Hold Head 4th materials');
+	Bones=NOTESKIN:GetPath('','arrowmesh');
+
+};
+
+return t;
