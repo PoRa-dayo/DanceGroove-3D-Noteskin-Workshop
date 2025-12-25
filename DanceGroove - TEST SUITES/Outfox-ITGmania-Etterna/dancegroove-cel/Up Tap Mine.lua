58805c0616a5adfc3dbdaf8e864e4eedf20e5563
PoRa-dayo <62925753+PoRa-dayo@users.noreply.github.com>
2025-12-26 00:14:21 +0700

    test suites

---
+++ DanceGroove - TEST SUITES/Outfox-ITGmania-Etterna/dancegroove-cel/Up Tap Mine.lua
@@ -0,0 +1,8 @@
+local t = Def.Model {
+	Meshes="_mine meshes.txt";
+	Materials="_mine materials.txt";
+	Bones="_mine bones.txt";
+};
+
+return t;
+
---
+++ DanceGroove - TEST SUITES/Outfox-ITGmania-Etterna/dancegroove-gold/Down Tap Lift.lua
@@ -0,0 +1,8 @@
+local t = Def.Model {
+	Meshes=NOTESKIN:GetPath('','liftmesh');
+	Materials=NOTESKIN:GetPath('','liftmesh');
+	Bones=NOTESKIN:GetPath('','liftmesh');
+
+};
+
+return t;
